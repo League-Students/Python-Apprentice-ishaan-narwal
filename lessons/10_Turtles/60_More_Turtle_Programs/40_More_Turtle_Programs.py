6f8eb1f8bb74defd37cb1d847d0b61576e4d288e
League Student <student@jointheleague.org>
2026-07-02 13:12:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -14,14 +14,6 @@ Use this code to get a random x and y location
 import random
     x = random.randint(-300, 300)
     y = random.randint(-300, 300)
-"""
-Copy the code from the previous lesson, 10_More_Turtle_Programs.ipynb,
-from the section "Change the Turtle's Image"
-
-Then change the code so that the turtle has a different image ( look in the 'images'
-directory ) and moves to the corners of the screen in a square pattern.
-"""
-
 import turtle                                           # Import the turtle module
 
 screen = turtle.Screen()                                    # Set up the screen
