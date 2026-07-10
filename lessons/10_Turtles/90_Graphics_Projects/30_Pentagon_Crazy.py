829ae697386f03666a111ded75a73cbf4946b326
League Student <student@jointheleague.org>
2026-07-10 13:25:29 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -31,25 +31,25 @@ def show_anamatronics():
 def open_cam_1():
     global cam_num
     print("CAM 1 OPEN")
-    
+    cam_num = 1
     screen.bgcolor(cam_colors[0])
     show_anamatronics()
 def open_cam_2():
     global cam_num
     print("CAM 2 OPEN")
-    
+    cam_num = 2
     screen.bgcolor(cam_colors[1])
     show_anamatronics()
 def open_cam_3():
     global cam_num
     print("CAM 2 OPEN")
-    
+    cam_num = 3
     screen.bgcolor(cam_colors[2])
     show_anamatronics()
 def open_cam_4():
     global cam_num
     print("CAM 2 OPEN")
-    
+    cam_num = 4
     screen.bgcolor(cam_colors[3])
     show_anamatronics()
     
