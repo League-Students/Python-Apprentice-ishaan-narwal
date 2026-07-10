3565947e0c2e8a828e9b8329c558cc7a62fa2b75
League Student <student@jointheleague.org>
2026-07-10 13:05:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -25,17 +25,20 @@ def show_anamatronics(cam_num):
 def open_cam_1():
     print("CAM 1 OPEN")
     screen.bgcolor(cam_colors[0])
-
+    show_anamatronics(cam_num)
 def open_cam_2():
     print("CAM 2 OPEN")
     screen.bgcolor(cam_colors[1])
-    
+    show_anamatronics(cam_num)
 def open_cam_3():
     print("CAM 2 OPEN")
     screen.bgcolor(cam_colors[2])
+    show_anamatronics(cam_num)
 def open_cam_4():
     print("CAM 2 OPEN")
     screen.bgcolor(cam_colors[3])
+    show_anamatronics(cam_num)
+    
 def open_cam_5():
     print("CAM 2 OPEN")
     screen.bgcolor(cam_colors[4])
