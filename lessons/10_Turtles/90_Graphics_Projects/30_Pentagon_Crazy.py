6d6adf522330ccebf178bdcad614b4287d2ce2be
League Student <student@jointheleague.org>
2026-07-10 12:39:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -5,11 +5,13 @@ tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(500, 500)
 
-cam_colors = ["red, "black", "white", "green",  ]
+cam_colors = ["red, "black", "white", "green", "blue" ]
 
 
 
 
 
+screen.listen()
+screen.onkey()
 
 turtle.exitonclick()
