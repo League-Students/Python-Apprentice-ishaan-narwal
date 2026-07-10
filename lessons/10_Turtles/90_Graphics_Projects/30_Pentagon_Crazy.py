7832a55146b4c0a3bbfa60289a15583bb458d910
League Student <student@jointheleague.org>
2026-07-10 13:27:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -15,6 +15,7 @@ cam_colors = ["red", "grey", "white", "green", "blue"]
 def move_tina():
     global tina_progress
     tina_progress += 1
+    tina
     shown_anamatronics()
     screen.ontimer(move_tina, 2000)
     
