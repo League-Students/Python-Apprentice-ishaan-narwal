2206f3acaf4e30529694b2a98ac6f69355d3b2b7
League Student <student@jointheleague.org>
2026-07-10 12:44:17 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -18,6 +18,8 @@ def open_cam_2():
 screen.listen()
 screen.onkey(open_cam_1, "1")
 screen.onkey(open_cam_2, "2")
-screen.onkey(open_cam_1, "1")screen.onkey(open_cam_1, "1")
+screen.onkey(open_cam_3, "1")
+screen.onkey(open_cam_4, "1")
+screen.onkey(open_cam_5, "4")
 
 turtle.exitonclick()
